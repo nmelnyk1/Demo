9e3909197af2059acca28491a4e69e8ec13dbfa1
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2020-10-28 15:40:26 +0200

    Worker

--- src/Employee.java
+++ src/Employee.java
@@ -1,60 +1,85 @@
 
 public class Employee {
-		private String firstname;
-		private String lastname;
+		private String name;
 		private int age;
+		private int salary;
+		private int hours;
 		public static int count;
-		static {
-			System.out.println("Static block");
-			count =0;
-		}
-		public Employee() {
-			firstname = "";
-			lastname ="";
-			age =0;
-			count++;
-		}
 		
-		public Employee(String firstname, String lastname, int age) {
-			
-			this.firstname = firstname;
-			this.lastname = lastname;
+		static {
+			System.out.println("Static Block");
+			count = 0;
+		}
+		public Employee(String name, int age, int salary, int hours) {
+			this.name = name;
 			this.age = age;
+			this.salary = salary;
+			this.hours = hours;
 			count++;
 		}
 
-		public String getFirstname() {
-			return firstname;
+
+		public Employee() {
+			name = "";
+			age =0;
+			salary =0;
+			hours =0;
+			count++;
 		}
 
-		public void setFirstname(String firstname) {
-			this.firstname = firstname;
+
+		public String getName() {
+			return name;
 		}
 
-		public String getLastname() {
-			return lastname;
+
+		public void setName(String name) {
+			this.name = name;
 		}
 
-		public void setLastname(String lastname) {
-			this.lastname = lastname;
-		}
 
 		public int getAge() {
 			return age;
 		}
 
+
 		public void setAge(int age) {
 			this.age = age;
 		}
+
+
+		public int getSalary() {
+			return salary;
+		}
+
+
+		public void setSalary(int salary) {
+			this.salary = salary;
+		}
+
+
+		public int getHours() {
+			return hours;
+		}
+
+
+		public void setHours(int hours) {
+			this.hours = hours;
+		}
+
 
 		public static int getCount() {
 			return count;
 		}
 
-		public  void setCount(int count) {
+
+		public static void setCount(int count) {
 			Employee.count = count;
 		}
-}
+		}
+
+		
+		
 		
 		
 
